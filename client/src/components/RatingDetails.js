3650e59a6ef3a26b41a1ec644af2a3bee77f19de
Kazuki Shigemitsu <kazuki.shigemitsu@gmail.com>
2019-04-07 12:35:16 -0700

    update the stylings of popup

--- client/src/components/RatingDetails.js
+++ client/src/components/RatingDetails.js
@@ -4,19 +4,18 @@ import FormatAlignLeft from '@material-ui/icons/FormatAlignLeft'
 import StarRatingComponent from 'react-star-rating-component';
 
 const RatingDetailsContainer = styled.div`
+  display: inline-block;
 `
 const RatingPopup = styled.div`
   display: ${props => props.isOpen ? 'block' : 'none' };
   position: absolute;
   top: 30px;
+  padding: 10px 12px;
   left:0;
-  min-width: 600px;
-  min-height: 100px;
   border: 5px solid rgb(215, 210, 196);
   border-radius: 5px;
   background: #fff;
   font-size: 12px;
-  font-weight: bold;
   color: #382110;
   font-family: "Lato", "Helvetica Neue", Helvetica, Arial, sans-serif;
 `
@@ -25,6 +24,12 @@ const OpenButton = styled.div`
   border: none;
   cursor: pointer;
   margin: 0 10px;
+
+  svg {
+    width: .8em;
+    height: .8em;
+    display: block;
+  }
 `
 
 const CloseButton = styled.div`
@@ -45,43 +50,47 @@ const BarContainer = styled.div`
   position: relative;
   margin: 3px;
   display: flex;
+  align-items: center;
   `
 
 const BarLine = styled.div`
-  min-height: 20px;
   width: 350px;
-  margin-left: 10px
+  margin: 0 10px;
 `
 const BarFill = styled.div`
   background-color: #215625;
   width: ${props => props.percent}%;
   height: 18px;
+  border-radius: 2px;
 `
 const FlexColumnReverse = styled.div`
   display: flex;
   flex-direction: column-reverse;
+  margin-top: 10px;
 `
 
 const SimpleText = styled.div`
   line-height: 24px
   font-size: 12px;
   color: #382110;
+  font-weight: bold;
   text-align: left;
   font-family: "Lato", "Helvetica Neue", "Helvetica", sans-serif;
 `
 class Bar extends Component {
   render(){
-    const { rating, votes, largestVotingNumber } = this.props;
+    const { ratings, rating, votes, largestVotingNumber, amountOfRatings } = this.props;
 
-    const percent = votes / largestVotingNumber * 100;
+    const percent = votes / amountOfRatings * 100;
+    const width = votes / largestVotingNumber * 100;
     return (
       <BarContainer>
        <SimpleText><span>{ rating }</span></SimpleText>
        <StarRatingComponent starCount={1} value={1}/>
        <BarLine>
-        <BarFill percent={percent} />
+        <BarFill percent={width} />
        </BarLine>
-       <SimpleText><span>{ Math.floor(percent) }% </span></SimpleText>
+       <SimpleText><span>{ Math.round(percent * 100) / 100 }% </span></SimpleText>
         <SimpleText><span>({ votes })</span></SimpleText>
       </BarContainer>
     )
@@ -97,7 +106,7 @@ class RatingDetails extends Component {
 
   render(){
     const { isOpen } = this.state;
-    const { ratings } = this.props;
+    const { ratings, rating, likedBy, reviews, users } = this.props;
 
     const ratingSummary = !ratings ? null : ratings.reduce((acc, v)=>{
       const upd = {};
@@ -124,21 +133,28 @@ class RatingDetails extends Component {
       return acc;
     }, 0);
 
-    console.log('largestVotingNumber', largestVotingNumber);
-
     return (
       <RatingDetailsContainer>
-        <OpenButton onClick={this.toggle}><FormatAlignLeft /></OpenButton>
+        <OpenButton onClick={this.toggle}><FormatAlignLeft/></OpenButton>
         <RatingPopup isOpen={isOpen}>
           <CloseButton onClick={this.toggle}>x</CloseButton>
-          <span>Rating Details</span>
+          <strong>Rating Details</strong>
           {
             !!ratings && <FlexColumnReverse>{
             Object.keys(ratingSummary).map( rating => {
-              return <Bar rating={rating} votes={ratingSummary[rating]} largestVotingNumber={largestVotingNumber} />
+              return <Bar rating={rating} votes={ratingSummary[rating]} largestVotingNumber={largestVotingNumber} amountOfRatings={ratings.length} />
             })
             }</FlexColumnReverse>
           }
+          <div>
+            <strong>{likedBy}%</strong> of people liked it
+          </div>
+          <div>
+            <strong>This edition:</strong> {rating} average rating,&nbsp;
+            {ratings.length} ratings,&nbsp;
+            {reviews.length} reviews,&nbsp;
+            {users.length} users
+          </div>
         </RatingPopup>
       </RatingDetailsContainer>
     )
